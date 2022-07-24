8207755b1da564ecc89f2c7e6ad0ee855c1bfb11
Longs Pemun Gotar <developerlongs@gmail.com>
2022-07-24 20:56:28 +0000

    added some comments

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function ethereumKeyGeneration() {
-    var ethers = require("ethers");
-    var crypto = require("crypto");
-    for (i = 0; i < 5; i++) {
-      var id = crypto.randomBytes(32).toString("hex");
-      var privateKey = "0x" + id;
-      console.log("This is your private key:", privateKey);
-      var wallet = new ethers.Wallet(privateKey);
-      console.log("Address: " + wallet.address);
-    }
+  // the ethers.js library
+  var ethers = require("ethers");
+
+  // the native crypto module in nodejs
+  var crypto = require("crypto");
+  for (i = 0; i < 5; i++) {
+    var id = crypto.randomBytes(32).toString("hex");
+    var privateKey = "0x" + id;
+    console.log("This is your private key:", privateKey);
+    var wallet = new ethers.Wallet(privateKey);
+    console.log("Address: " + wallet.address);
   }
-  ethereumKeyGeneration();
-  
+}
+ethereumKeyGeneration();
